7ecb5116c06a173b1932c7b86e56a644636cd726
mametarou967 <mametarou967@gmail.com>
2022-08-20 22:09:56 +0900

    画面A,Bの追加

---
+++ WpfScreenBack/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using WpfScreenBack.Views;
+
+namespace WpfScreenBack.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public DelegateCommand ShowViewAButton { get; }
+        void ShowViewAButtonExecuter() => regionManager.RequestNavigate("ContentRegion", nameof(ViewA));
+        public DelegateCommand ShowViewBButton { get; }
+        void ShowViewBButtonExecuter() => regionManager.RequestNavigate("ContentRegion", nameof(ViewB));
+
+        private readonly IRegionManager regionManager;
+
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+            ShowViewAButton = new DelegateCommand(ShowViewAButtonExecuter);
+            ShowViewBButton = new DelegateCommand(ShowViewBButtonExecuter);
+        }
+    }
+}
