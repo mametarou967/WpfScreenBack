b9eee3a73fbe772ed62fefbf16ed08eb6f8468ac
mametarou967 <mametarou967@gmail.com>
2022-08-20 23:07:37 +0900

    ScreenTimerのクラス化

---
+++ WpfScreenBack/Models/ScreenTimer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace WpfScreenBack.Models
+{
+    /// <summary>
+    /// 主に画面表示時に、時間(ミリ秒)と処理内容（画面遷移）などを与えておくことで、
+    /// 指定した時間経過後に該当の処理を発動させるクラスです
+    /// 時間を延長させたい場合はUpdateメソッドを呼んでください(例えば、画面タッチ時など)
+    /// また、画面を遷移する際にはかならずScreenTimer.dipose()を呼ぶようにしてください。
+    /// 理由としてはタイマーが残ったままになってしまう可能性があるので、画面遷移後も時間満了後に
+    /// この処理が走ってしまう可能性があります
+    /// </summary>
+    public class ScreenTimer : IDisposable
+    {
+        private DispatcherTimer timer;
+        int triggerMillisecond;
+        Action executionContentsOnTrigger;
+
+        private bool disposedValue;
+
+        public ScreenTimer(int triggerMillisecond,Action executionContentsOnTrigger)
+        {
+            this.triggerMillisecond = triggerMillisecond;
+            this.executionContentsOnTrigger = executionContentsOnTrigger;
+            TimerReset();
+        }
+
+        public void Update()
+        {
+            TimerReset();
+        }
+
+        private void TimerReset()
+        {
+            // 既にタイマーがある場合にはそのタイマーは止めておく
+            if (timer != null)
+            {
+                timer.Stop();
+            }
+
+            // 新しいタイマーの時間をセットする
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(triggerMillisecond)
+            };
+
+            // タイマーが満了した時の処理を追加する
+            timer.Tick += (sender, e) =>
+            {
+                // 直接触っても例外が起きない
+                this.executionContentsOnTrigger();
+            };
+
+            // タイマーの計測を開始する
+            this.timer.Start();
+        }
+
+        private void TimerStop()
+        {
+            // 既にタイマーがある場合にはそのタイマーは止めておく
+            if (this.timer != null)
+            {
+                this.timer.Stop();
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                    TimerStop();
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~ScreenTimer()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- WpfScreenBack/ViewModels/ViewBViewModel.cs
+++ WpfScreenBack/ViewModels/ViewBViewModel.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
+using WpfScreenBack.Models;
 using WpfScreenBack.Views;
 
 namespace WpfScreenBack.ViewModels
@@ -15,68 +16,44 @@ namespace WpfScreenBack.ViewModels
     public class ViewBViewModel : BindableBase , INavigationAware
     {
         private readonly IRegionManager regionManager;
-        public DelegateCommand ScreenTouchCommand { get; }
 
-        void ScreenTouchCommandExecuter()
-        {
-            TimerReset();
-        }
 
         public ViewBViewModel(IRegionManager regionManager)
         {
             this.regionManager = regionManager;
-            ScreenTouchCommand = new DelegateCommand(ScreenTouchCommandExecuter);
+            ScreenTouchCommand = new DelegateCommand(ScreenTouchCommandExecuter); // これも忘れずに
         }
 
-        public bool IsNavigationTarget(NavigationContext navigationContext) => false; 
-        
-        public void OnNavigatedFrom(NavigationContext navigationContext)
-        {
-            TimerStop();
-        }
+        public bool IsNavigationTarget(NavigationContext navigationContext) => false;
 
+        // -------------------------------------------------------------------------------- // 
+        ScreenTimer screenTimer;
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
-            TimerReset();
+            // この画面に移ってきた際に時間と、時間が満了したときの処理を付与して、スクリーンタイマーを生成しておく
+            // 生成すると同時にタイマーは自動的に開始する
+            // 今回は例として画面Bで3秒経ったら画面Aに遷移にするものとする
+            screenTimer = new ScreenTimer(3000,() => regionManager.RequestNavigate("ContentRegion", nameof(ViewA)));
         }
 
-        // タイマーのセット
-        // タイマーのリセット
-        // タイマーの破棄
-        private DispatcherTimer timer;
-
-        void TimerReset()
+        public void OnNavigatedFrom(NavigationContext navigationContext)
         {
-            // 既にタイマーがある場合にはそのタイマーは止めておく
-            if(this.timer != null)
-            {
-                this.timer.Stop();
-            }
-
-            // 新しいタイマーの時間をセットする
-            this.timer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(3000)
-            };
-
-            // タイマーが満了した時の処理を追加する
-            this.timer.Tick += (sender, e) =>
-            {
-                // 直接触っても例外が起きない
-                regionManager.RequestNavigate("ContentRegion", nameof(ViewA));
-            };
-
-            // タイマーの計測を開始する
-            this.timer.Start();
+            // 何らかの条件でこの画面から遷移する際には、スクリーンタイマーは止めておく必要があるので
+            // 忘れずに破棄しておく
+            screenTimer?.Dispose();
         }
 
-        void TimerStop()
+        // View側で一定領域を画面タッチすると以下のコマンドが発行されるように定義してある
+        public DelegateCommand ScreenTouchCommand { get; }
+        void ScreenTouchCommandExecuter()
         {
-            // 既にタイマーがある場合にはそのタイマーは止めておく
-            if (this.timer != null)
-            {
-                this.timer.Stop();
-            }
+            // UserControl領域のどこかをタッチされたら、スクリーンタイマーはUpdate(=時間を初期化)
+            // しておく、これにより、一定時間内に画面を触られている限りは処理は発動しない
+            // このサンプルでは画面B(緑)を3秒以内にポチポチしていれば画面Aには遷移しない
+            // 画面Bの文字の外側はUserControlの外側なので、この部分をポチポチしていても画面Aに遷移してしまう
+            // これを防ごうとすると画面Bの画面領域を広げないといけないと思われる(x,yを指定するという意味)
+            // これを頑張るかはこだわり次第と思う（もっといい方法があるかも...)
+            screenTimer?.Update(); 
         }
     }
 }
